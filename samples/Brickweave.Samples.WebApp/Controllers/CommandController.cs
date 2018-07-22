e32c9b9c1f2237e850846fe48cc5c8648b5dcc06
Adam Gartee <agartee@gmail.com>
2018-07-22 18:05:33 -0400

    re-added authorization attribute in samples app

--- samples/Brickweave.Samples.WebApp/Controllers/CommandController.cs
+++ samples/Brickweave.Samples.WebApp/Controllers/CommandController.cs
@@ -3,11 +3,12 @@ using System.Threading.Tasks;
 using Brickweave.Cqrs.Cli;
 using Brickweave.Cqrs.Cli.Formatters;
 using Brickweave.Cqrs.Cli.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Brickweave.Samples.WebApp.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class CommandController : Controller
     {
         private readonly ICliDispatcher _cliDispatcher;

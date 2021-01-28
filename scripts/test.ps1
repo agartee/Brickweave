f7c4b22ba38994d8c5944522c2fa5fc8a165a47a
Adam Gartee <agartee@gmail.com>
2021-01-28 09:11:53 -0500

    updated test.ps1 for build server

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,10 +1,14 @@
+Param(
+  [Parameter(Mandatory=$false, HelpMessage="Configuration name (e.g. Release, Debug)")]
+  [string]$configuration = "Debug"
+)
 $status = 0
 $rootDir = (get-item $PSScriptRoot).Parent.FullName
 $testProjects = Get-ChildItem -Path $rootDir\tests -Filter *.csproj -Recurse -File| ForEach-Object { $_.FullName }
 
 foreach ($testProject in $testProjects)
 {
-    dotnet test $testProject --no-build
+    dotnet test $testProject --no-build -c $configuration
 
     if ($status -eq 0) {
         $status = $LASTEXITCODE

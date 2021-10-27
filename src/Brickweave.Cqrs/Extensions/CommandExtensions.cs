21b707d1bbac28298ffb17201692c87d179e3b5e
Adam Gartee <agartee@gmail.com>
2021-10-27 08:37:14 -0400

    before dispatcher updates

---
+++ src/Brickweave.Cqrs/Extensions/CommandExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Brickweave.Cqrs.Attributes;
+
+namespace Brickweave.Cqrs.Extensions
+{
+    public static class CommandExtensions
+    {
+        public static bool IsLongRunning(this ICommand executable)
+        {
+            return Attribute.IsDefined(executable.GetType(), typeof(LongRunningAttribute));
+        }
+    }
+}
